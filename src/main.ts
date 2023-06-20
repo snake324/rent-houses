1b1a7944c6886c453b2f153137aae6116bf70daf
snake324 <kristi4n90@gmail.com>
2023-06-20 23:04:37 +0200

    until lesson 14- continue on step 2

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,3 @@
-/*
-*  Protractor support is deprecated in Angular.
-*  Protractor is used in this example for compatibility with Angular documentation tools.
-*/
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
